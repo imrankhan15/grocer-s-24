e11feb3339242f5aa9a5901d32c798f99186d32c
imrankhan15 <buetcse110@gmail.com>
2018-03-05 23:36:22 +0600

    First commit

---
+++ Everyday's Grocery/LogOutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LogOutViewController.swift
+//  Everyday's Grocery
+//
+//  Created by Muhammad Faisal Imran Khan on 1/17/18.
+//  Copyright © 2018 MI Apps. All rights reserved.
+//
+
+import UIKit
+
+class LogOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func unwindToLoginView(segue: UIStoryboardSegue) {
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
